fab9c0793d3e9380dd1fd404d56055c28e1b7a4c
Cai Lehwald <cailehwald@googlemail.com>
2017-10-13 01:02:33 +0100

    Add Tutorial Source files, implement basic game core javascript. Modify home page to include phaser and core game javascript. modify home page to disaply game window in a panel, with panel for instructions

--- Phaser First Game Tutorial/Phaser First Game Tutorial/Controllers/HomeController.cs
+++ Phaser First Game Tutorial/Phaser First Game Tutorial/Controllers/HomeController.cs
@@ -12,19 +12,5 @@ namespace Phaser_First_Game_Tutorial.Controllers
         {
             return View();
         }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
