7f88a6994f8e6158b46d26455b15f729d363f6d4
Cai Lehwald <cailehwald@googlemail.com>
2017-10-13 01:33:44 +0100

    Load game assets, add a star sprite to the game window. Add some commenting. Tweak home page

--- Phaser First Game Tutorial/Phaser First Game Tutorial/Scripts/Game Core/GameController.js
+++ Phaser First Game Tutorial/Phaser First Game Tutorial/Scripts/Game Core/GameController.js
@@ -1,9 +1,17 @@
-﻿var game = new Phaser.Game(800, 600, Phaser.AUTO, 'GameWindow', { preload: preload, create: create, update: update });
+﻿// Initialise the phaser game window, give it a width of 800 and a height of 600, set the rendering context to auto and attach the window to a div with the ID "GameWindow"
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'GameWindow', { preload: preload, create: create, update: update });
 
 function preload() {
+    // Load game assets
+    game.load.image('sky', '../Content/Assets/sky.png');
+    game.load.image('ground', '../Content/Assets/platform.png');
+    game.load.image('star', '../Content/Assets/star.png');
+    game.load.spritesheet('dude', '../Content/Assets/dude.png', 32, 48);
 }
 
 function create() {
+    // Create a sprite at 0,0 using the star asset
+    game.add.sprite(0, 0, 'star');
 }
 
 function update() {
